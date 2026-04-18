03a6f84b934b125d9a56636b50a973d68dceb187
aymenpro7773-ship-it <aymenpro7773@gmail.com>
2026-04-18 20:12:19 +0000

    fix: remove unresolved kotlinOptions from root build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,11 @@
+buildscript {
+    repositories { google(); mavenCentral() }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
+    }
+}
+
 allprojects {
     repositories { google(); mavenCentral() }
-    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-        kotlinOptions.jvmTarget = "17"
-    }
 }
